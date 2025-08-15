18c1de5c00815c986b48b3e8a400b6146a152988
LorenzoTaylor <lmtaylor9@dons.usfca.edu>
2025-08-15 16:19:19 -0700

    fix: make modal initial size the same as first component size

--- packages/renumerate/dist/renumerate.es.js
+++ packages/renumerate/dist/renumerate.es.js
@@ -213,7 +213,7 @@ class a {
 			.renumerate-dialog-content iframe {
 				width: 100%;
 				height: 100%;
-				min-height: 160px;
+				min-height: 304px;
 				min-width: 412x;
 				border: none;
 				margin: 0;
